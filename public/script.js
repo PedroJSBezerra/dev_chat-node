f02c095a030133a25a1e7dc854efc15144971fea
PedroJSBezerra <pjsoares46@gmail.com>
2021-08-11 18:04:49 -0300

    chat log update

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
 var socket = io()
 let chat = document.querySelector('#chat')
+let chatLog = document.querySelector('.chatLog')
 
 function showMessage(msg) {
-    console.log(msg)
+    // console.log(msg)
+    chatLog.insertAdjacentHTML("beforeend", `<li><strong>${msg.username}: </strong>${msg.usermessage}</li>`)
 }
 
 socket.on('serverSendMsg', (msg) => {
@@ -11,6 +13,7 @@ socket.on('serverSendMsg', (msg) => {
 
 chat.addEventListener('submit', (event) => {
     event.preventDefault()
+
     let username = document.querySelector('input[name=username]').value
     let usermessage = document.querySelector('input[name=usermessage]').value
 
