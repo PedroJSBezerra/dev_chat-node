c482d3c167b12101c5b99ca2349b14e418820c9e
Pedro Jorge Soares Bezerra <52613930+PedroJSBezerra@users.noreply.github.com>
2021-08-11 12:00:50 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const path = require('path')
+
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'public'))
+    .engine('html', require('ejs').renderFile)
+    .set('view engine', 'html')
+
+    .use('/', (req,res) => {
+        res.render('index.html')
+    })
+
+let messages = []
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`)
+
+    socket.emit('previousMessages', messages)
+
+    socket.on('sendMessage', data => {
+        messages.push(data)
+        socket.broadcast.emit('receivedMessage', data)
+    })
+})
+
+server.listen(3000)
