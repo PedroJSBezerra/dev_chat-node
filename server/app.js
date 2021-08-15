8f5382f95a6fda8fb82e737b8c289b4ea6befd97
PedroJSBezerra <pjsoares46@gmail.com>
2021-08-15 19:29:47 -0300

    back->cors front->form event

--- server/app.js
+++ server/app.js
@@ -1,21 +1,26 @@
 const express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io')(server),
-    path = require('path'),
+    io = require('socket.io')(server, {
+        //permissão para cross origin resouce (cors)
+        cors: {
+            origin: '*',
+        }
+    }),
+    //path = require('path'),
     socket = require('./socket.js'),
     host = 'localhost',
     port = 3001
 
-app
-    .use(express.static(path.join(__dirname, '../public')))
-    .set('views', path.join(__dirname, '../public'))
-    .engine('html', require('ejs').renderFile)
-    .set('view engine', 'html')
+// app  //config for static server pages
+//     .use(express.static(path.join(__dirname, '../public')))
+//     .set('views', path.join(__dirname, '../public'))
+//     .engine('html', require('ejs').renderFile)
+//     .set('view engine', 'html')
 
-    .use('/', (req, res) => {
-        res.render('index.html')
-    })
+//     .use('/', (req, res) => {
+//         res.render('index.html')
+//     })
 
 socket.start(io)
-server.listen(port, console.log(`Node Server listen http://${host}:${port}`))
+server.listen(port, console.log(`Server Node->express->Socket.io listen on http://${host}:${port}`))
--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React, {useState} from 'react'
+//import React, {useState} from 'react'
+import io from 'socket.io-client'
 import './App.css'
 
+const socket = io('http://localhost:3001')
+socket.on('connection', () => {
+  console.log('Socket.io connected!')
+})
+
 function App() {
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    let message = event.target.firstChild.value
+
+    let msgObject = {
+      username: 'Pedro',
+      usermessage: message
+    }
+
+    console.log(msgObject)
+    //clear input field
+    event.target.reset()
+  }
+
   return (
     <div className="App">
       <div className="header">
@@ -17,7 +38,7 @@ function App() {
           <li><strong>Sala 01 diz: </strong>Seja bem vindo(a) ao bate papo!</li>
         </div>
       </main>
-      <form className="messagearea">
+      <form className="messagearea" onSubmit={handleFormSubmit}>
         <input type="text" name="usermessage" placeholder="Digite sua mensagen"/>
         <button type="submit">
           <img className="menu" width="30px" height="30px" src="" alt=""/>
